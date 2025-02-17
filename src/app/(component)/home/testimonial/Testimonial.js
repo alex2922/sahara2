7b67703f5cf037d07d14b19979ba2768340b971c
Harsh Singh <alexlin2922@gmail.com>
2025-02-17 17:43:40 +0530

    new

--- src/app/(component)/home/testimonial/Testimonial.js
+++ src/app/(component)/home/testimonial/Testimonial.js
@@ -1,12 +1,69 @@
-import React from 'react'
-import "./Testimonial.scss"
+"use client";
+
+import React from "react";
+import "./Testimonial.scss";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css/bundle";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import { Star } from "lucide-react";
 
 const Testimonial = () => {
-  return (
-    <div className="testimonials parent">
-        <div className="conatiner testimonials-container"></div>
-      </div>
-  )
-}
+  const data = [
+    {
+      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, eaque reiciendis magnam quia corporis eveniet recusandae.",
+      name: "Customer Name",
+    },
+    {
+      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, eaque reiciendis magnam quia corporis eveniet recusandae.",
+      name: "Customer Name",
+    },
+    {
+      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, eaque reiciendis magnam quia corporis eveniet recusandae.",
+      name: "Customer Name",
+    }
+  ];
 
-export default Testimonial
+  return (
+    <div className="testimonials">
+      <div className="container testimonials-container">
+        <h2>
+          What <span>Our Visitors</span> Say
+        </h2>
+
+        <div className="testimonial-swiper">
+          <Swiper
+            spaceBetween={20}
+            slidesPerView={1}
+            breakpoints={{
+              400: { slidesPerView: 1, spaceBetween: 20 },
+              768: { slidesPerView: 2, spaceBetween: 20 },
+              1024: { slidesPerView: 3, spaceBetween: 20 },
+            }}
+            autoplay={{ delay: 2500, disableOnInteraction: true }}
+            pagination={{ clickable: true }}
+            navigation={true}
+            modules={[Autoplay, Pagination, Navigation]}
+            className="mySwiper"
+          >
+            {data.map((testimonial, index) => (
+              <SwiperSlide key={index}>
+                <div className="testimonialcard">
+                  <div className="stars">
+                    {[...Array(5)].map((_, i) => (
+                      <Star key={i} size={20} fill="var(--accent)" stroke="none" />
+                    ))}
+                  </div>
+
+                  <p>{testimonial.text}</p>
+                  <strong>{testimonial.name}</strong>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonial;
--- src/app/(component)/swiper/ActivitySlider.js
+++ src/app/(component)/swiper/ActivitySlider.js
@@ -4,24 +4,64 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
-
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 function ActivityCard(props) {
   return (
     <div className="card">
       <div className="imgbox"></div>
-      <h2>Go <span>Karting</span></h2>
-      <p>Experience the thrill of racing on our state-of-the-art karting tracks, designed for speed enthusiasts of all levels.</p>
+      <h2>
+        {props.title} <span> {props.title_highlight}</span>
+      </h2>
+      <p>
+        Experience the thrill of racing on our state-of-the-art karting tracks,
+        designed for speed enthusiasts of all levels.
+      </p>
       <div className="btns">
-        <a href="#" className="btn fstbtn">Book Now</a>
-        <a href="#" className="btn2 sndbtn">Learn More</a>
+        
+        <a href="#" className="btn2 sndbtn">
+          Learn More
+        </a>
+        <a href="#" className="btn fstbtn">
+          Book Now
+        </a>
       </div>
     </div>
   );
 }
 
-
+const data = [
+  {
+    id: 1,
+    title: "Go",
+    title_highlight: "karting",
+    desc: "Experience the thrill of racing on our state-of-the-art karting tracks, designed for speed enthusiasts of all levels.",
+  },
+  {
+    id: 2,
+    title: "Quad",
+    title_highlight: "Bike",
+    desc: "Experience the thrill of racing on our state-of-the-art karting tracks, designed for speed enthusiasts of all levels.",
+  },
+  {
+    id: 3,
+    title: "Dune ",
+    title_highlight: "Buggy",
+    desc: "Experience the thrill of racing on our state-of-the-art karting tracks, designed for speed enthusiasts of all levels.",
+  },
+  {
+    id: 4,
+    title: "Mini",
+    title_highlight: "GP",
+    desc: "Experience the thrill of racing on our state-of-the-art karting tracks, designed for speed enthusiasts of all levels.",
+  },
+  {
+    id: 5,
+    title: "Paintball",
+    title_highlight: " Fight",
+    desc: "Experience the thrill of racing on our state-of-the-art karting tracks, designed for speed enthusiasts of all levels.",
+  },
+];
 
 function ActivitySlider() {
   return (
@@ -55,13 +95,11 @@ function ActivitySlider() {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide><ActivityCard /></SwiperSlide>
-        <SwiperSlide><ActivityCard /></SwiperSlide>
-        <SwiperSlide><ActivityCard /></SwiperSlide>
-        <SwiperSlide><ActivityCard /></SwiperSlide>
-        <SwiperSlide><ActivityCard /></SwiperSlide>
-
-
+        {data.map((card, index) => (
+          <SwiperSlide key={index}>
+            <ActivityCard title={card.title}  title_highlight={card.title_highlight} />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </>
   );
