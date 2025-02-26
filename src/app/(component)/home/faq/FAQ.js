94527fa30ee12680d24ed6a1ec6d973d4fac3be5
Nitakarkhile <nitagawali22@gmail.com>
2025-02-26 14:04:04 +0530

    FAQ

--- src/app/(component)/home/faq/FAQ.js
+++ src/app/(component)/home/faq/FAQ.js
@@ -1,12 +1,69 @@
-import React from 'react'
-import "./FAQ.scss"
+"use client";
+
+import React, { useState } from "react";
+import "./FAQ.scss";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 
 const FAQ = () => {
-  return (
-    <div className="faq parent">
-    <div className="conatiner faq-container"></div>
-  </div>
-  )
-}
+  const [activeIndex, setActiveIndex] = useState(0);
 
-export default FAQ
+  const toggleAccordion = (index) => {
+    setActiveIndex(index); // Set the active index to the clicked one
+  };
+
+  const accordianData = [
+    {
+      title: "Lorem ipsum dolor sit amet1",
+      content:
+        "lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae nisi praesentium ipsam!",
+    },
+    {
+      title: "2Lorem ipsum dolor sit amet1",
+      content:
+        "lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae nisi praesentium ipsam!",
+    },
+    {
+      title: "Three Lorem ipsum dolor sit amet1",
+      content:
+        "lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae nisi praesentium ipsam!",
+    },
+    {
+      title: "Four orem ipsum dolor sit amet1",
+      content:
+        "lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae nisi praesentium ipsam!",
+    },
+  ];
+
+  return (
+    <>
+      <div className="faq parent">
+        <div className="container faq-container">
+          <h2>
+            Plan Your <span>Visit</span>
+          </h2>
+         <div className="faq-box-container">
+         {accordianData.map((item, index) => (
+            <div className="faq-box" key={index}>
+              <div
+                className={`accordian-ques ${activeIndex === index ? "active" : ""}`}
+                onClick={() => toggleAccordion(index)}
+              >
+                <p className="accordian-p">{item.title}</p>
+                <span>
+                  {activeIndex === index ? <IoIosArrowUp /> : <IoIosArrowDown />}
+                </span>
+              </div>
+
+              {activeIndex === index && (
+                <p className="accordian-ans">{item.content}</p>
+              )}
+            </div>
+          ))}
+         </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default FAQ;
