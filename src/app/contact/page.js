4c1660a8fca4d87d1be961201b64b1b81b4da3a5
Harsh Singh <alexlin2922@gmail.com>
2025-03-06 13:55:18 +0530

    234

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,16 +1,56 @@
 import React from 'react'
 import "./contact.scss"
+import BlackBorder from '../(component)/border/BlackBorder'
+import GreyBorder from '../(component)/border/GreyBorder'
 
 const page = () => {
   return (
-    <div className="contact parent">
-      <div className="contact-container container">
-        <h1>asdasdasdasd</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id officiis natus suscipit quae illo aliquam non, nihil laborum accusantium facere facilis impedit! Quasi molestias, corrupti dolor neque obcaecati in fugit vitae vero nesciunt. Perspiciatis magni non, temporibus tempora nisi delectus exercitationem. Asperiores ullam inventore enim voluptate ratione. Earum labore nostrum tempora tenetur, vitae possimus. Ab, vero laudantium reprehenderit quo soluta provident rem dignissimos. Placeat, aut saepe sed possimus ducimus, necessitatibus perferendis incidunt doloribus nihil, quisquam enim fugit quis recusandae. Expedita neque odit voluptatibus! Nobis fugit recusandae soluta eveniet, pariatur asperiores itaque voluptate alias ratione voluptatem, corrupti iure rem quaerat minima. Unde dolore ipsa quos, beatae cupiditate doloremque placeat harum aut expedita nulla a modi? Impedit quasi vitae eum illum sit. Accusantium totam ea libero perspiciatis harum molestias nemo eaque, reiciendis vel officiis culpa maiores quis, numquam quaerat. Fugiat maiores est, ducimus quod, animi atque error corporis molestiae, omnis distinctio eligendi id ut accusantium velit quaerat enim laudantium aliquid aliquam ipsum laborum iure dicta dolorem. Corporis rerum velit facere asperiores ullam ratione, deserunt necessitatibus illo sapiente at, blanditiis delectus ducimus error quia ea id, inventore eos odio. Unde cupiditate inventore ad at quasi ipsum sapiente repellendus amet aliquam odio accusantium, quas adipisci repudiandae facilis culpa iure quisquam pariatur quia delectus. Architecto doloribus eius dolores quisquam corrupti
-        </p>
+    <>
+      <div className="contact1 parent">
+        <div className="contact-container1 container">
+          <h1>Let’s Get in Touch!</h1>
+
+          <p>Have questions or ready to book your adventure? Reach out to us! Our team is here to help you plan the ultimate experience with Sahara Amusement.</p>
+
+
+
+
+
+        </div>
       </div>
-    </div>
+
+
+      <BlackBorder flip={true} margin={"-96px"} />
+
+      <div className="contact2 parent">
+        <div className="contact-container2 container">
+
+
+          <div className="left">
+            <h1>Let’s Get in Touch!</h1>
+
+            <p>Have questions or ready to book your adventure? Reach out to us! Our team is here to help you plan the ultimate experience with Sahara Amusement.</p>
+
+          </div>
+
+          <div className="right">
+
+            <form action=""> 
+
+
+
+            </form>
+
+          </div>
+
+
+        </div>
+      </div>
+
+      <GreyBorder  margin={"-96px"} />
+
+
+    </>
   )
 }
 
